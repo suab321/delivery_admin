c51594cc98f00647ea749094b1d08958b7efa43e
Abhinav Singh <adityasaha890@gmail.com>
2019-04-24 20:41:33 +0530

    g

--- drivers/driver.js
+++ drivers/driver.js
@@ -25,6 +25,25 @@ const get_token=(req,res,next)=>{
         res.status(400).json({msg:"Token is required",response:"1"})
 }
 
+//route to get unverified users//
+router.get('/get_unverified_user',get_token,(req,res)=>{
+    const user_id=decodeToken(req.token).user;
+    if(user_id){
+        user_model.findById({_id:user_id}).then(user=>{
+            axios.get(`${driver}/services/get_pending_drivers`).then(user=>{
+                res.status(200).json(user);
+            }).catch(err=>{
+                res.status(400).json({msg:"unable to fetch data",response:"1"});
+            })
+        }).catch(err=>{
+            res.status(400).json({msg:"you are not a valid user",response:"2"});
+        })
+    }
+    else
+        res.status(400).json({msg:"You are not authenticated user",response:"3"});
+})
+//route ended///
+
 //route to get all drivers//
 router.get('/get_drivers',get_token,(req,res)=>{
     const user_id=decodeToken(req.token).user;
@@ -64,11 +83,11 @@ router.get('/get_pending_orders',get_token,(req,res)=>{
 //route ended///
 
 //route to get all orders
-router.get('/get_pending_orders',get_token,(req,res)=>{
+router.get('/get_orders',get_token,(req,res)=>{
     const user_id=decodeToken(req.token).user;
     if(user_id){
         user_model.findById({_id:user_id}).then(user=>{
-            axios.get(`${driver}/services/pending_order)`).then(user=>{
+            axios.get(`${driver}/services/order)`).then(user=>{
                 res.status(200).json(user);
             }).catch(err=>{
                 res.status(400).json({msg:"Error in from driver side",response:"1"});
